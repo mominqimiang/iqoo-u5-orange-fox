5b37c68f9ef914a56b35ed3a8e8c957f8463a71b
mominqimiang <50542889+mominqimiang@users.noreply.github.com>
2023-04-06 07:01:04 +0800

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-export ALLOW_MISSING_DEPENDENCIES=true
-export LC_ALL="C"
